91e3e055d49c8a49a1a60cf00d7c471e89574bcc
pghorpade <pghorpade@library.ucla.edu>
2024-10-09 18:15:23 -0700

    feat: update lbguides url

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,5 +1,5 @@
 export default async () => {
-  const apiUrl = 'https://api.libguides.com/1.1/your-endpoint' // Replace with your external API URL
+  const apiUrl = 'https://lgapi-us.libapps.com/widget_box.php?site_id=705&widget_type=8&output_format=2&widget_title=Library+Alert&widget_height=250&widget_width=100%25&guide_id=1427138&box_id=33325708&map_id=39190263&content_only=0&include_jquery=1&config_id=1727199122170' // Replace with your external API URL
 
   try {
     // Fetch data from external API
