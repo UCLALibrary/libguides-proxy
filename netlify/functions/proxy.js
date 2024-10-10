7fe58f3bd3cf345208cad47ebbd8beccbaf89e2f
pghorpade <pghorpade@library.ucla.edu>
2024-10-10 10:13:11 -0700

    feat: remove node-fetch and fix the proxy code

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,6 +1,4 @@
-import fetch from 'node-fetch';
-
-exports.handler = async () => {
+export default async () => {
   const apiUrl = 'https://lgapi-us.libapps.com/widget_box.php?site_id=705&widget_type=8&output_format=2&widget_title=Library+Alert&widget_height=250&widget_width=100%25&guide_id=1427138&box_id=33325708&map_id=39190263&content_only=0&include_jquery=1&config_id=1727199122170';
 
   try {
@@ -9,13 +7,12 @@ exports.handler = async () => {
     const data = await response.text(); // Using .text() because the content might not be JSON
 
     // Return the response from the external API
-    return {
-      statusCode: 200,
-      body: data,
-      headers: {
-        'Content-Type': 'text/html' // Adjust if necessary
-      }
-    };
+    return new Response(
+       data,{
+        headers: {'Content-Type': 'text/html'},
+        status: 200
+       }
+      )
   } catch (error) {
     // Handle any errors
     return {
