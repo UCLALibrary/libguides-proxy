c51c131dae8535ed5f0346f43dee81cb2a06556a
pghorpade <pghorpade@library.ucla.edu>
2024-10-25 17:10:05 -0700

    feat: use dom parser

--- netlify/functions/globalDataProxy.js
+++ netlify/functions/globalDataProxy.js
@@ -1,15 +1,28 @@
+import { parseFromString } from 'dom-parser'
+
+const globals = {
+  libraryAlert: {
+    title: "Alert",
+    text: ""
+  },
+  fullBannerAlert:{
+    title: "Alert",
+    text: ""
+  }
+};
 export default async (request) => {
   // We have not built a component for this yet for the main library website
-  const globalLibraryAlertBannerApiUrl = 'https://uclalibrary.slack.com//lgapi-us.libapps.com/widgets.php?site_id=705&widget_type=8&output_format=1&widget_embed_type=2&guide_id=1427138&box_id=33325708&map_id=39190263&content_only=0&include_jquery=1&config_id=1729804564421'; 
-  const globalLibraryAlertDismissableBoxApiUrl = 'https://uclalibrary.slack.com//lgapi-us.libapps.com/widgets.php?site_id=705&widget_type=8&output_format=1&widget_embed_type=2&guide_id=1427138&box_id=33464938&map_id=39354139&content_only=0&include_jquery=1&config_id=1729804592797'; 
-  const libraryStatusUpdateSection1ApiUrl = 'https://uclalibrary.slack.com//lgapi-us.libapps.com/widgets.php?site_id=705&widget_type=8&output_format=1&widget_embed_type=2&guide_id=1427138&box_id=33464944&map_id=39354145&content_only=0&include_jquery=1&config_id=1729804600932'; 
-  const libraryStatusUpdateSection2ApiUrl = 'https://uclalibrary.slack.com//lgapi-us.libapps.com/widgets.php?site_id=705&widget_type=8&output_format=1&widget_embed_type=2&guide_id=1427138&box_id=33464982&map_id=39354184&content_only=0&include_jquery=1&config_id=1729804613204'; 
+  const globalLibraryAlertBannerApiUrl = 'https://lgapi-us.libapps.com/widgets.php?site_id=705&widget_type=8&output_format=1&widget_embed_type=2&guide_id=1427138&box_id=33325708&map_id=39190263&content_only=0&include_jquery=1&config_id=1729804564421'; 
+  const globalLibraryAlertDismissableBoxApiUrl = 'https://lgapi-us.libapps.com/widgets.php?site_id=705&widget_type=8&output_format=1&widget_embed_type=2&guide_id=1427138&box_id=33464938&map_id=39354139&content_only=0&include_jquery=1&config_id=1729804592797'; 
+ 
   
   const CORS_HEADERS = {
     'Access-Control-Allow-Origin': '*', // Allows all origins (you can restrict this to specific domains)
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type'
   };
+  
+
   try {
     // Fetch data from external API
     if (request.method === 'OPTIONS') {
@@ -17,14 +30,28 @@ export default async (request) => {
         headers: CORS_HEADERS
       });
     }
-    const response = await fetch(apiUrl);
-    const data = await response.text(); // Using .text() because the content might not be JSON
-
+    // Fetch data from both sections
+    const [globalLibraryAlertBanner, globalLibraryAlertDismissableBox] = await Promise.all([
+      fetch(globalLibraryAlertBannerApiUrl),
+      fetch(globalLibraryAlertDismissableBoxApiUrl),
+    ]);
+    console.log("globals")
+    const dataSection1 = await globalLibraryAlertBanner.text();
+    const dataSection2 = await globalLibraryAlertDismissableBox.text();
+    // Parse the HTML
+    const document1 = parseFromString(dataSection1, 'text/html');
+    // globals.fullBannerAlert.title = document1.getElementsByTagName('h3')[0]?.textContent;
+    globals.fullBannerAlert.text = document1.getElementById('s-lg-content-78281771').innerHTML;
+    // Parse the HTML
+    const document2 = parseFromString(dataSection2, 'text/html');
+    // globals.libraryAlert.title = document2.getElementsByTagName('h3')[0].textContent;
+    globals.libraryAlert.text = document2.getElementById('s-lg-content-78601329').innerHTML;
+    console.log(globals)
     // Return the response from the external API
     return new Response(
-       data,{
+      JSON.stringify(globals),{
         headers: {
-          'Content-Type': 'text/html',
+          'Content-Type': 'application/json',
           ...CORS_HEADERS
         },
         status: 200
